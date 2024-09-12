9d9395bcb2cc3a295097f16498fec4939ee9e288
Olena Dats <olena.dats@screeningeagle.com>
2024-09-12 15:33:27 +0200

    Improve scroll to top code

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-window.onscroll = function() {
+window.onload = function() {
     scrollFunction();
+};
+
+window.onscroll = function() {
+  scrollFunction();
 };
 
 
 function scrollFunction() {
-    const scrollToTop = document.getElementById("scrollToTop");
+    let scrollToTop = document.getElementById("scrollToTop");
 
   if (document.body.scrollTop > document.body.scrollHeight / 2 || document.documentElement.scrollTop > document.documentElement.scrollHeight / 2) {
     scrollToTop.style.display = "block";
   } else {
     scrollToTop.style.display = "none";
-}
-}
+  }
+};
+
